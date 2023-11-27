3ed86ff5d443ce1e1a2e3c35fcae51c9d6bbc749
viertel97 <janik.viertel@outlook.com>
2023-11-27 08:15:47 +0100

    fixed keyerror

--- services/todoist.py
+++ services/todoist.py
@@ -2,6 +2,7 @@ import os
 from quarter_lib.akeyless import get_secrets
 import requests
 from quarter_lib.logging import setup_logging
+
 TODOIST_TOKEN = get_secrets(["todoist/token"])
 
 logger = setup_logging(__file__)
@@ -12,7 +13,7 @@ def get_current_offset():
     return requests.post(
         "https://api.todoist.com/sync/v9/sync",
         headers={
-            "Authorization": f"Bearer {os.environ['TODOIST_TOKEN']}",
+            "Authorization": f"Bearer {TODOIST_TOKEN}",
             "Content-Type": "application/x-www-form-urlencoded",
         },
         data={"sync_token": "*", "resource_types": '["user"]'},
