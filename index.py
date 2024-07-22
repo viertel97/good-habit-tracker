df09be13deb180cf0f5f5bcdb90ae9d275861d34
Janik Viertel <janik@next-data-service.com>
2024-07-22 17:56:15 +0200

    added new pathnames

--- index.py
+++ index.py
@@ -20,7 +20,9 @@ app.layout = html.Div(
 # Create the callback to handle mutlipage inputs
 @app.callback(Output("page-content", "children"), [Input("url", "pathname")])
 def display_page(pathname):
-    if pathname == "/daily":
+    if pathname == "/daily/morning":
+        return default.generate_layout()
+    if pathname == "/daily/evening":
         return default.generate_layout()
     if pathname == "/weekly":
         return default.generate_layout()
@@ -33,7 +35,10 @@ def display_page(pathname):
     else:  # if redirected to unknown link
         return html.Div(
             [
-                dcc.Link("Daily", href="/daily"),
+                dcc.Link("Daily - Morning", href="/daily/morning"),
+                html.Br(),
+                dcc.Link("Daily - Evening", href="/daily/evening"),
+                html.Br(),
                 html.Br(),
                 dcc.Link("Weekly", href="/weekly"),
                 html.Br(),
@@ -48,4 +53,4 @@ def display_page(pathname):
 
 # Run the app on localhost:8050
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=False, port=8050)
+    app.run(host="0.0.0.0", debug=True, port=8050)
